c1f9632c4144c2adfaa3b697838994c1e6891228
Nicolas Ordoñez Chala <nordonezc@unal.edu.co>
2023-09-12 17:20:52 -0500

    Implement Application Service Layer

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/dto/create/CreateOrderCommand.java
@@ -0,0 +1,27 @@
+package com.food.ordering.system.order.service.domain.dto.create;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class CreateOrderCommand {
+
+    @NotNull
+    UUID customerId;
+    @NotNull
+    UUID restaurantId;
+    @NotNull
+    BigDecimal price;
+    @NotNull
+    List<OrderItem> orderItems;
+    @NotNull
+    OrderAddress address;
+}
---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/dto/create/CreateOrderResponse.java
@@ -0,0 +1,22 @@
+package com.food.ordering.system.order.service.domain.dto.create;
+
+import com.food.ordering.system.domain.valueobject.OrderStatus;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class CreateOrderResponse {
+
+    @NotNull
+    UUID orderTrackingId;
+    @NotNull
+    OrderStatus orderStatus;
+    @NotNull
+    String message;
+}
