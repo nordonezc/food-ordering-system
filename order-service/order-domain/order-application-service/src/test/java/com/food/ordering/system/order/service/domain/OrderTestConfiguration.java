7cf5cde4ba52f854e02a8b51c8a5aa8cdded3f53
Nicolas Ordoñez Chala <nordonezc@unal.edu.co>
2023-10-05 20:30:59 -0500

    Add infrastructure customization with docker and docker compose

--- order-service/order-domain/order-application-service/src/test/java/com/food/ordering/system/order/service/domain/OrderTestConfiguration.java
+++ order-service/order-domain/order-application-service/src/test/java/com/food/ordering/system/order/service/domain/OrderTestConfiguration.java
@@ -17,37 +17,37 @@ import org.springframework.context.annotation.Bean;
 public class OrderTestConfiguration {
 
     @Bean
-    public OrderCreatedPaymentRequestMessagePublisher orderCreatedPaymentRequestMessagePublisher(){
+    public OrderCreatedPaymentRequestMessagePublisher orderCreatedPaymentRequestMessagePublisher() {
         return Mockito.mock(OrderCreatedPaymentRequestMessagePublisher.class);
     }
 
     @Bean
-    public OrderCancelledPaymentRequestMessagePublisher orderCancelledPaymentRequestMessagePublisher(){
+    public OrderCancelledPaymentRequestMessagePublisher orderCancelledPaymentRequestMessagePublisher() {
         return Mockito.mock(OrderCancelledPaymentRequestMessagePublisher.class);
     }
 
     @Bean
-    public OrderPaidRestaurantRequestMessagePublisher orderPaidRestaurantRequestMessagePublisher(){
+    public OrderPaidRestaurantRequestMessagePublisher orderPaidRestaurantRequestMessagePublisher() {
         return Mockito.mock(OrderPaidRestaurantRequestMessagePublisher.class);
     }
 
     @Bean
-    public OrderRepository orderRepository(){
+    public OrderRepository orderRepository() {
         return Mockito.mock(OrderRepository.class);
     }
 
     @Bean
-    public CustomerRepository customerRepository(){
+    public CustomerRepository customerRepository() {
         return Mockito.mock(CustomerRepository.class);
     }
 
     @Bean
-    public RestaurantRepository restaurantRepository(){
+    public RestaurantRepository restaurantRepository() {
         return Mockito.mock(RestaurantRepository.class);
     }
 
     @Bean
-    public OrderDomainService orderDomainService(){
+    public OrderDomainService orderDomainService() {
         return new OrderDomainServiceImpl();
     }
 
